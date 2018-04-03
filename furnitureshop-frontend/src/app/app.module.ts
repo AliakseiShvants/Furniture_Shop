846ad472155810a41d27fcc044e16693e0ac99dc
Aliaksei Shvants <ashvants91@gmail.com>
2018-04-03 23:44:23 +0300

    UI: created header and navbar component

--- furnitureshop-frontend/src/app/app.module.ts
+++ furnitureshop-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import {RoutingModule} from './routing/routing.module';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
