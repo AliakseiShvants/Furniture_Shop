a0cdd4cbcc625aa799285153dad302d9b87002ff
Aliaksei Shvants <ashvants91@gmail.com>
2018-04-04 18:08:27 +0300

    created view component complete header, footer, menu.component added translate support

--- furnitureshop-frontend/src/app/app.module.ts
+++ furnitureshop-frontend/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import {RoutingModule} from './routing/routing.module';
 import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { RegisterComponent } from './register/register.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {AlertModule, ModalModule} from 'ngx-bootstrap';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -14,13 +22,31 @@ import { MenuComponent } from './menu/menu.component';
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    AboutComponent,
+    ViewerComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
+    HttpClientModule,
+    AlertModule.forRoot(),
+    ModalModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
--- furnitureshop-frontend/src/app/routing/routing.module.ts
+++ furnitureshop-frontend/src/app/routing/routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
+import {AboutComponent} from '../about/about.component';
+import {ViewerComponent} from '../viewer/viewer.component';
 
 export const appRoutes: Routes = [
-
+  {path: 'about', component: AboutComponent},
+  {path: 'view', component: ViewerComponent},
+  { path: '',   redirectTo: '/about', pathMatch: 'full' },
 ];
 
 @NgModule({
